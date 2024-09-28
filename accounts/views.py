7b7ec876d549ca1ab8d3cd2063f3d449e375760f
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-09-28 07:15:44 +0300

    customize templates accounts and create UserProfile Model

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
 # Create your views here.
 
 def signin(request):
-    return render(request,'accounts/signin.html')
+
+    if request.method=='POST' and 'btnlogin' in request.POST:
+
+        messages.info(request,'this is post and btn')
+        return redirect('accounts:signin')
+    else:
+        return render(request,'accounts/signin.html')
 
 def signup(request):
-    return render(request,'accounts/signup.html')
+
+    if request.method=='POST' and 'btnsignup' in request.POST:
+
+        messages.info(request,'this is post and btn')
+        return redirect('accounts:signup')
+    else:
+        return render(request,'accounts/signup.html')
 
 def profile(request):
-    return render(request,'accounts/profile.html')
+    
+    if request.method=='POST' and 'btnsave' in request.POST:
+
+        messages.info(request,'this is post and btn')
+        return redirect('accounts:profile')
+    else:
+        return render(request,'accounts/profile.html')
